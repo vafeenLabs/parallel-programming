fe74656f109850ac700d691355161cc018a2f79f
Vafeen <666av6@gmail.com>
2024-11-08 14:04:01 +0300

    modify tasks type

--- lab2/ThreadPool.cpp
+++ lab2/ThreadPool.cpp
@@ -1,67 +1,55 @@
 #include "ThreadPool.hpp"
 
+// Метод, выполняющий задачи в потоках
 void ThreadPool::run()
 {
     while (true)
     {
-        std::function<void(int)> task;
-
-        {
-            std::unique_lock<std::mutex> lock(queueMutex);
-            condition.wait(lock, [this]
-                           { return stop || !tasks.empty(); });
-            if (stop && tasks.empty())
-                return;
-            task = std::move(tasks.front());
-            tasks.pop();
-        }
-
-        task(0);
+        std::unique_lock<std::mutex> lock(queueMutex);
+        condition.wait(lock, [this]
+                       { return stop || !tasks.empty(); });
+        if (stop && tasks.empty())
+            return;
+        std::function<void()> task = std::move(tasks.front());
+        tasks.pop();
+        task();
     }
 }
 
 // Метод для добавления задачи в пул и получения результата через future
-std::future<void> ThreadPool::enqueue(std::function<void(int)> task)
+std::future<void> ThreadPool::enqueue(std::function<void()> task)
 {
-    auto taskPtr = std::make_shared<std::packaged_task<void(int)>>(std::move(task));
+    auto taskPtr = std::make_shared<std::packaged_task<void()>>(std::move(task));
     std::future<void> res = taskPtr->get_future();
 
     {
-        // std::unique_lock<std::mutex> lock(queueMutex);
         std::lock_guard<std::mutex> lock(queueMutex);
         if (stop)
             throw std::runtime_error("enqueue on stopped ThreadPool");
-        tasks.emplace([taskPtr](int id)
-                      { (*taskPtr)(id); });
+        tasks.emplace([taskPtr]()
+                      { (*taskPtr)(); });
     }
     condition.notify_one();
     return res;
 }
 
-ThreadPool::ThreadPool() : stop(false)
+// Конструктор для инициализации пула потоков с заданным количеством потоков
+ThreadPool::ThreadPool(size_t threads) : stop(false)
 {
-    size_t numThreads = std::thread::hardware_concurrency();
-    for (size_t i = 0; i < numThreads; ++i)
-    {
+    for (size_t i = 0; i < threads; ++i)
         workers.emplace_back([this]
                              { run(); });
-    }
 }
 
-// Деструктор для завершения работы потоков
+// Деструктор завершает работу потоков
 ThreadPool::~ThreadPool()
 {
     {
         std::lock_guard<std::mutex> lock(queueMutex);
-        // std::unique_lock<std::mutex> lock(queueMutex);
         stop = true;
     }
     condition.notify_all();
     for (std::thread &worker : workers)
-    {
         if (worker.joinable())
-        {
             worker.join();
-        }
-    }
 }
--- lab2/ThreadPool.hpp
+++ lab2/ThreadPool.hpp
@@ -12,10 +12,10 @@
 class ThreadPool
 {
 public:
-    // Конструктор, инициализирующий пул потоков с заданным количеством потоков
-    ThreadPool();
+    // Конструктор для инициализации пула потоков с заданным количеством потоков
+    ThreadPool(size_t threads = std::thread::hardware_concurrency());
 
-    // Деструктор для завершения работы потоков
+    // Деструктор завершает работу потоков
     ~ThreadPool();
 
     ThreadPool(const ThreadPool &) = delete;
@@ -24,14 +24,14 @@ public:
     ThreadPool &operator=(ThreadPool &&) = delete;
 
     // Метод для добавления задачи в пул и получения результата через future
-    std::future<void> enqueue(std::function<void(int)> task);
+    std::future<void> enqueue(std::function<void()> task);
 
 private:
-    // Метод, который выполняется в каждом потоке
+    // Метод, выполняющий задачи в потоках
     void run();
 
     std::vector<std::thread> workers;           // Вектор рабочих потоков
-    std::queue<std::function<void(int)>> tasks; // Очередь задач
+    std::queue<std::function<void()>> tasks; // Очередь задач
     std::mutex queueMutex;                      // Мьютекс для защиты очереди задач
     std::condition_variable condition;          // Условная переменная для синхронизации
     bool stop;                                  // Флаг остановки пула потоков
